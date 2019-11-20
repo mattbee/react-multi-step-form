6d06a0804b7f7ab4a2182724e554e50f9727ad2e
Matt Bee <matt@todayishould.com>
2019-11-20 21:34:58 +0000

    Extract validation helpers to separate file
    The main component file is getting a bit busy. This commit moves the
    validation functions to a helper folder, and exports them as named
    exports, for use in the main component.

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,7 @@ import React, { useState } from 'react';
 import Layout from '../components/Layout';
 import InputWithLabel from '../components/InputWithLabel';
 import Button from '../components/Button';
-
-const nameValid = (name) => {
-  if (name.length < 2) {
-    return 'Name is too short';
-  } else if (name.length >= 25) {
-    return 'Name is too long';
-  }
-
-  return null;
-};
-
-const emailValid = (email) => {
-  const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (email.length == 0) {
-    return 'Please enter your email'
-  } else if (!emailRegex.test(email)) {
-    return 'Please check your email address';
-  }
-
-  return null;
-}
+import { nameValid, emailValid } from '../helpers/validation';
 
 const NAME_STEP = 'name';
 const EMAIL_STEP = 'email';
