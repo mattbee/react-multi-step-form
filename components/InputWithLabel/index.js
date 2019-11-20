7ae1bde5298ef181dc12db1abd9c828095131e31
Matt Bee <matt@todayishould.com>
2019-11-20 00:46:10 +0000

    Very basic styles for input component
    This adds some really simple styles for the input component.

--- components/InputWithLabel/index.js
+++ components/InputWithLabel/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './InputWithLabel.module.scss';
 
 const InputWithLabel = ({
   label,
@@ -20,6 +21,7 @@ const InputWithLabel = ({
         onChange={onChange}
         placeholder={placeholder}
         error={error}
+        className={styles.input}
       />
     </label>
     {error && <div>{error}</div>}
