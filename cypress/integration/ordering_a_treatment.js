5fffffdae45af5a9ab80044dd2f4b41ea322b173
Matt Bee <matt@todayishould.com>
2019-11-20 00:53:08 +0000

    Introduce FormState for Name field
    The idea is that the form will essential be controlled by the basic
    state machine `INITIAL_STATE` which will decide page content, handle
    validation and errors, pass all the relevant information to the form
    inputs and also control the journey through the form.
    
    With this structure we can simply add to the `INITIAL_STATE` object and
    further form fields on their own pages will 'just work'.
    
    This will make testing easier as we can basically decide on the contract
    between the from object and what should be displayed in the specs very
    easily (easier than getting jest working with NextJS).
    
    The handleChange function should work for any type of input, as the
    `event` object is fairly standard for getting values etc from any form
    field.
    
    Initially this will only work with the `InputWithLabel` component but it
    should be possible to enable a Radio button group later.

--- cypress/integration/ordering_a_treatment.js
+++ cypress/integration/ordering_a_treatment.js
@@ -2,5 +2,13 @@ describe('SH:24 Code Test', function() {
   it('Visits the Ordering page', function() {
     cy.visit('http://localhost:3000');
     cy.contains('SH:24 Order Journey')
-  })
+  });
+
+  it('Can order a product', function() {
+    cy.visit('http://localhost:3000');
+
+    cy.get('input[name=name]')
+      .type('Colt Seavers')
+      .should('have.value', 'Colt Seavers');
+  });
 })
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/Layout';
+import InputWithLabel from '../components/InputWithLabel';
+import Button from '../components/Button';
 
-const MultiStepForm = () => (
-  <Layout>
-    <p>Order from SH:24</p>
-  </Layout>
-);
+const NAME_STEP = 'name';
+
+const INITIAL_STATE = {
+  [NAME_STEP]: {
+    pageTitle: 'What is your name?',
+    name: 'name',
+    label: 'Name:',
+    type: 'text',
+    error: '',
+    value: '',
+    nextStep: null,
+  },
+};
+
+const MultiStepForm = ({ initialStep = NAME_STEP }) => {
+  const [formStep, setFormStep] = useState(initialStep);
+  const [formState, setFormState] = useState(INITIAL_STATE);
+
+  const handleChange = (e) => {
+    setFormState({
+      ...formState,
+      [formStep]: {
+        ...formState[formStep],
+        value: e.currentTarget.value
+      }
+    });
+  };
+
+  const onNext = (e) => {
+    e.preventDefault();
+  };
+
+  const currentStep = formState[formStep];
+
+  return (
+    <Layout>
+      <form action="" method="post" style={{ maxWidth: '20em' }}>
+        <h2>{currentStep.pageTitle}</h2>
+        <InputWithLabel
+          {...currentStep}
+          onChange={handleChange}
+        />
+
+        <div style={{ display: 'flex', justifyContent: 'space-between', margin: '2em 0' }}>
+          <Button>Prev</Button>
+          <Button onClick={onNext}>Next</Button>
+        </div>
+      </form>
+    </Layout>
+  );
+};
 
 export default MultiStepForm;
